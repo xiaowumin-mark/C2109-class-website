89be84f940e1167d5d7c79195909b1121b111986
xiaowumin-mark <xiaowumin-mark@outlook.com>
2023-12-10 11:20:49 +0800

    first commit

--- pc/script.js
+++ pc/script.js
@@ -16,7 +16,15 @@ modeSwitch.addEventListener('click',()=>{
     body.classList.toggle('dark');
     if(body.classList.contains('dark')){
         modeText.innerText="浅色模式"
+        setCookie("page_mode","true",60)
     }else{
         modeText.innerText="暗黑模式"
+        setCookie("page_mode","false",60)
     }
 })
+
+window.onload = function(){
+    if (getCookie("page_mode") != "false"){
+        body.classList.toggle('dark')
+    }
+}
