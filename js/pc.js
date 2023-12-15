74905b16c28d2c1e1a4d5fcde800b3d623f7398a
xiaowumin-mark <xiaowumin-mark@outlook.com>
2023-12-16 07:47:59 +0800

    first commit

--- js/pc.js
+++ js/pc.js
@@ -1,20 +1,22 @@
-let tab = ["home", "about", "tz", "img", "ts"]
-for (i = 0; i < tab.length; i++) {
-    if (getUrlParams('tab')==tab[i]){
-        console.log(tab[i])
-    }
-}
-
-$('#myModal').modal({
-    backdrop: 'static',
-    keyboard: false
-});
-$.ajax({
-    url: '../modal.json',
-    type: 'get',
-    success: function (res) {
-        if (res.v) {
-            ShowModal(res.btn, res.main, res.title)
+window.onload = function () {
+    let tab = ["home", "about", "tz", "img", "ts"]
+    for (i = 0; i < tab.length; i++) {
+        if (getUrlParams('tab') == tab[i]) {
+            console.log(tab[i])
         }
     }
-});
+
+    $('#myModal').modal({
+        backdrop: 'static',
+        keyboard: false
+    });
+    $.ajax({
+        url: '../modal.json',
+        type: 'get',
+        success: function (res) {
+            if (res.v) {
+                ShowModal(res.btn, res.main, res.title)
+            }
+        }
+    });
+}
